82178a0af1eef12b3c38eebfa4aada646dcffa51
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-06-22 11:42:23 +0600

    Updated

---
+++ Express Routes/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const useRouter = require('./routes/users.routes');
+
+app.use('/api/user', useRouter)
+
+app.get("/", (req, res) => {
+  res.send("<h1>Im get Home at App</h1>")
+})
+app.use((req, res) => {
+  res.send("<h1>404 this page not found</h1>")
+})
+
+
+module.exports = app;
